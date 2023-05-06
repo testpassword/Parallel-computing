a5cb09f42ca577c3e3e5c4a62d7b39a841fac07c
Artemy Kulbako <kulbakowork@mail.ru>
2023-05-07 02:44:51 +0300

    fix extra_task build script

--- lab4-09.04.23/extra_task/build.sh
+++ lab4-09.04.23/extra_task/build.sh
@@ -1,4 +1,4 @@
 # Индивидуальное задание: проверить, есть ли разница при использвовании множества '#pragma omp parallel for' vs '#pragma omp parallel sections'
 
-gcc -O3 -Wall -o extra *.c -lm -fopenmp
+gcc -O3 -Wall -o extra extra_task/*.c -lm -fopenmp
 chmod +x extra
